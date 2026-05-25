2e9ff0c3f1302fff88a68586a79711a43f01fbd0
BloodConnect Developer <developer@bloodconnect.com>
2026-05-25 16:46:49 +0530

    Remove test scripts causing Vercel build failure

--- server/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function testFirebase() {
-    console.log("Firebase loaded");
-}
-testFirebase().catch(console.error);
--- server/test.ts
+++
@@ -1,8 +0,0 @@
-import { admin, db } from "./src/firebase";
-import bcrypt from "bcryptjs";
-
-async function testFirebase() {
-  console.log("Firebase loaded");
-}
-
-testFirebase().catch(console.error);
